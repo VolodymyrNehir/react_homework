10e09bd978cc3e2708692e30d1531accab1eb765
VolodymyrNehir <volodymyrnehir@gmail.com>
2022-01-25 21:55:50 +0200

    add

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {Route, Routes,Outlet, NavLink,BrowserRouter} from 'react-router-dom';
+import layout from './Layout.css';;
+// import posts from '/src/pages/Posts/Posts';
+
+const Layout = () => {
+    return (
+        <div className='layout.layout'>
+            hhhhh
+            {/*<link to='/posts' ></link>*/}
+{/*<NavLink to='/posts'>Posts</NavLink>*/}
+{/*           <Outlet/>*/}
+
+        </div>
+    );
+};
+
+export default Layout;
---
+++ src/pages/Posts/Posts.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'react-router-dom';
+import 'src/components/Layout/Layout';
+
+const Posts = () => {
+    return (
+        <div>
+hhhh
+        </div>
+    );
+};
+
+export default Posts;
