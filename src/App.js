9a5526076d91acbe84b53f5ac9fc3ef1744e3d5b
VolodymyrNehir <volodymyrnehir@gmail.com>
2022-01-30 20:32:39 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import {useReducer} from "react";
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'plus':
+            return {...state, dollar: state.dollar + 1}
+        case 'minus':
+            return {...state, dollar: state.dollar - 1}
+        case 'reset':
+            return {...state, dollar: action.payload}
+        case 'plusCent':
+            return {...state, cent: state.cent + 1}
+        case 'minusCent':
+            return {...state, cent: state.cent - 1}
+        case 'resetCent':
+            return {...state, cent: action.payload}
+        case 'plushundredsDollars':
+            return {...state, hundredsDollars: state.hundredsDollars + 1}
+        case 'minushundredsDollars':
+            return {...state, hundredsDollars: state.hundredsDollars - 1}
+        case 'resethundredsDollars':
+            return {...state, hundredsDollars: action.payload}
+    }
+
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [inc, setInc] = useReducer(reducer, {dollar: 0, cent: 0, hundredsDollars: 0})
+    return (
+        <div className="App">
+            <div className='hundredsDollars'>
+                hundreds of dollars
+                <div>{inc.hundredsDollars}00</div>
+                <button onClick={() => setInc({type: 'plushundredsDollars'})}>+</button>
+                <button onClick={() => setInc({type: 'minushundredsDollars'})}>-</button>
+                <button onClick={() => setInc({type: 'resethundredsDollars', payload: 0})}>reset</button>
+
+            </div>
+            <div className='dollar'> dollar
+                <div> {inc.dollar}</div>
+                <button onClick={() => setInc({type: 'plus'})}>+</button>
+                <button onClick={() => setInc({type: 'minus'})}>-</button>
+                <button onClick={() => setInc({type: 'reset', payload: 0})}>reset</button>
+            </div>
+            <div className='cent'>
+                cent
+                <div>{inc.cent}</div>
+                <button onClick={() => setInc({type: 'plusCent'})}>+</button>
+                <button onClick={() => setInc({type: 'minusCent'})}>-</button>
+                <button onClick={() => setInc({type: 'resetCent', payload: 0})}>reset</button>
+            </div>
+
+
+        </div>
+    );
 }
 
 export default App;
